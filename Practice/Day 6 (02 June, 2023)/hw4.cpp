ac3c13c674c8648638d39de06a7cdf4882cb05bf
Prateek <psr23803@gmail.com>
2023-06-02 20:15:00 +0530

    Today's C++ practice

---
+++ Practice/Day 6 (02 June, 2023)/hw4.cpp
@@ -0,0 +1,12 @@
+// Program to test the working of for-loop
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i= 10; i>5; i--)
+    {
+        cout<< i<<" ";
+        i--;
+    }
+}
