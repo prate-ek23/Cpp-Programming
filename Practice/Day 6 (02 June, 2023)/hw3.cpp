bfe4c47e35f8c55cd1e04a2a404598f4c8646f0c
Prateek <psr23803@gmail.com>
2023-06-02 20:14:55 +0530

    Today's C++ practice

---
+++ Practice/Day 6 (02 June, 2023)/hw3.cpp
@@ -0,0 +1,16 @@
+// Program to test the working of for-loop and 'continue' keyword
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i= 0; i<=15; i+=2)
+    {
+        cout<< i<<" ";
+        
+        if(i&1)
+        continue;
+
+        i++;
+    }
+}
