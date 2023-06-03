92e92eb4d20eb71651a5a7a0f3790fa36b77fbc3
Prateek <psr23803@gmail.com>
2023-06-03 20:23:31 +0530

    C++ practice Day 7

---
+++ Practice/Day 7 (03 June, 2023)/11.cpp
@@ -0,0 +1,23 @@
+// Program to convert decimal number to binary
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int ans = 0;
+    int i =0;
+
+    while(n!=0)
+    {
+        int bit = n&1;
+        ans = (bit * pow(10,i)) + i;
+
+        n = n>>1;
+        i++;
+    }
+    cout<< "Answer is: " << ans << endl;
+}
