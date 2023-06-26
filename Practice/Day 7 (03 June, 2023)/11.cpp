d923d743ef64568f1b1fb54e9615a57d298ed8ff
Prateek <psr23803@gmail.com>
2023-06-26 21:35:18 +0530

    edit

--- Practice/Day 7 (03 June, 2023)/11.cpp
+++ Practice/Day 7 (03 June, 2023)/11.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int ans = 0;
     int i =0;
 
     while(n!=0)
     {
-        int bit = n&1;
-        ans = (bit * pow(10,i)) + i;
+        int bit = n & 1;
+        ans = (bit * pow(10,i)) + ans;
 
         n = n>>1;
         i++;
     }
-    cout<< "Answer is: " << ans << endl;
+    cout<< "Answer is: " << ans<< endl;
 }
