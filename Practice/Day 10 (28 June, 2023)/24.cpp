0116631fb0744bf1ed8a0f070f63bd4c367543e1
Prateek <psr23803@gmail.com>
2023-06-29 00:14:00 +0530

    +1

---
+++ Practice/Day 10 (28 June, 2023)/24.cpp
@@ -0,0 +1,21 @@
+// Program to test pass by value using functions
+
+#include<iostream>
+using namespace std;
+
+void dummy(int n)
+{
+    n++;
+    cout << "n is " << n << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    dummy(n);
+
+    cout << "number is " << n << endl;
+    return 0;
+}
