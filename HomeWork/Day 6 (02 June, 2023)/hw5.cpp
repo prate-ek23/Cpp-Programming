fee1357e6d2507edd05e3b1e944bd50d8a9e1a87
Prateek <psr23803@gmail.com>
2023-06-30 21:35:30 +0530

    updation

---
+++ HomeWork/Day 6 (02 June, 2023)/hw5.cpp
@@ -0,0 +1,12 @@
+// Program to test the working of for-loop
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i= 0; i<=5; i++)
+    {
+        cout<< i<<" ";
+        i++;
+    }
+}
