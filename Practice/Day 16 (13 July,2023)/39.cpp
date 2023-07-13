ee7525264542fb8ce8d0ce8993166e279f07fb82
Prateek <psr23803@gmail.com>
2023-07-13 22:21:09 +0530

    C++ practice

---
+++ Practice/Day 16 (13 July,2023)/39.cpp
@@ -0,0 +1,29 @@
+// Program to find pivot element in a sorted and rotated array
+
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[], int n)
+{
+    int s=0, e=n-1;
+    int mid = s+ (e-s)/2;
+
+    while(s<e)
+    {
+        if(arr[mid] >= arr[0])
+        s = mid+1;
+
+        else
+        e = mid;
+
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
+
+int main ()
+{
+    int arr[5] = {3,8,10,17,1};
+
+    cout << "Index of Pivot is: " << getPivot(arr,5)<< endl;
+}
